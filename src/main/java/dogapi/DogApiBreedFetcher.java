b2296b252b26790fac0dc4361f7df1f8e0aee6a8
abdullah2056 <abdullah_2056@hotmail.com>
2025-10-18 13:21:02 -0400

    completed task1

--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -24,12 +24,39 @@ public class DogApiBreedFetcher implements BreedFetcher {
      * @throws BreedNotFoundException if the breed does not exist (or if the API call fails for any reason)
      */
     @Override
-    public List<String> getSubBreeds(String breed) {
+    public List<String> getSubBreeds(String breed) throws BreedNotFoundException {
+        String url = "https://dog.ceo/api/breed/" + breed + "/list";
+
+        Request request = new Request.Builder().url(url).get().build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful() ||  response.body() == null) {
+                throw new BreedNotFoundException("Breed not found (main breed does not exist)");
+            }
+
+            String body = response.body().string();
+            JSONObject json = new JSONObject(body);
+            String status = json.getString("status");
+
+            if (status.equals("error")) {
+                throw new BreedNotFoundException("Breed not found (main breed does not exist)");
+            }
+
+            JSONArray JSON_arr = json.getJSONArray("message");
+            List<String> subBreeds = new ArrayList<>();
+            for (int i = 0; i < JSON_arr.length(); i++) {
+                subBreeds.add(JSON_arr.getString(i));
+            }
+            return subBreeds;
+
+        }  catch (IOException e) {
+            throw new BreedNotFoundException("Breed not found (main breed does not exist)");
+        }
+
         // TODO Task 1: Complete this method based on its provided documentation
         //      and the documentation for the dog.ceo API. You may find it helpful
         //      to refer to the examples of using OkHttpClient from the last lab,
         //      as well as the code for parsing JSON responses.
         // return statement included so that the starter code can compile and run.
-        return new ArrayList<>();
     }
 }
